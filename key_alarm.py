5dd6cd2aedc5ab4ffe1ab9ae113e531580c04721
Shashank Mishra <shashank-mishra@hotmail.com>
2020-06-15 02:12:07 +0530

    Version 1.0
    Added new features:
    1. Save settings in a config file.
    2. Press shift+x to stop the background thread execution.
    3. New status bar

--- key_alarm.py
+++ key_alarm.py
@@ -5,10 +5,44 @@ import keyboard
 from datetime import datetime, timedelta
 import time
 import winsound
+import json
+import sys
 
-trigger_key = "A"
-countdown_timer = "00:00:00"
-alarm_time = "10"
+trigger_key = ""
+countdown_timer = ""
+alarm_time = ""
+trigger_thread = None
+
+def write_config_file(key, countdown, alarm):
+    try:
+        config = {
+            "trigger_key" : key,
+            "countdown_timer" : countdown,
+            "alarm_time" : alarm
+        }
+        with open("config.json", "w") as output_file:
+            json.dump(config, output_file)
+        print ('Created config.json')
+    except Exception as ex:
+        print ("Error!")
+        print (ex)
+
+def initial_config_load():
+    try:
+        with open('config.json', 'r') as input_file:
+            config = json.load(input_file)
+        
+        global trigger_key, countdown_timer, alarm_time
+        trigger_key = config['trigger_key']
+        countdown_timer = config['countdown_timer']
+        alarm_time = config['alarm_time']
+    except IOError as io_error:
+        if io_error.errno == 2:
+            write_config_file("F3","00:01:00","10")
+        initial_config_load()
+    except Exception as ex:
+        print ("Error!")
+        print (ex)
 
 def disable_input():
     trigger_key_input.config(state = 'disabled')
@@ -20,53 +54,70 @@ def save(key, countdown, alarm):
     trigger_key = key
     countdown_timer = countdown
     alarm_time = alarm
-    disable_input()
-    trigger()
+    write_config_file(key, countdown, alarm)
 
 def trigger():
+    global trigger_thread
+    disable_input()
     split_time = countdown_timer.split(":")
     cnt_dwn_hours = int(split_time[0])
     cnt_dwn_minutes = int(split_time[1])
     cnt_dwn_seconds = int(split_time[2])
     while True:
         if keyboard.is_pressed(trigger_key):
-            print ("Triggered")
+            status_label.configure(text = "Triggered")
             now = datetime.now()
             alarm_at = now + timedelta(hours=cnt_dwn_hours, minutes=cnt_dwn_minutes, seconds=cnt_dwn_seconds)
             delay = (alarm_at - now).total_seconds()
-            threading.Timer(delay, alarm).start()
+            trigger_thread = threading.Timer(delay, alarm)
+            trigger_thread.start()
+            winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
+            break
+        if keyboard.is_pressed('shift+x'):
+            winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
+            if trigger_thread != None:
+                trigger_thread = None
+                status_label.configure(text = "Thread closed")
+            status_label.configure(text = "Press trigger button to start again!!")
             break
 
 def alarm():
-    print('Alarm!')
-    winsound.PlaySound('alarm.wav', winsound.SND_ASYNC)
+    status_label.configure(text = "Alarm!!")
+    winsound.PlaySound('./assets/alarm.wav', winsound.SND_ASYNC)
     loop_timer = time.time() + int(alarm_time)
     while True:
         if time.time() > loop_timer:
             winsound.PlaySound(None, winsound.SND_PURGE)
+            status_label.configure(text = "Press " + trigger_key + " to start again or shift+x to stop the background process.")
             break
     trigger()
-    print ("Done")
+
+initial_config_load()
 
 root = Tk()
 root.title('Key Alarm')
-root.geometry('400x150')
-icon = PhotoImage(file = 'alarm.png')
+root.geometry('400x220')
+icon = PhotoImage(file = './assets/alarm.png')
 root.iconphoto(False, icon)
 
 trigger_key_label = Label(root, text = "Trigger key", width = 22, anchor = 'w')
 trigger_key_input = Entry(root, width = 20)
-trigger_key_input.insert(0, "A")
+trigger_key_input.insert(0, trigger_key)
 
 countdown_timer_label = Label(root, text = "Countdown time (hh:mm:ss)", width = 22, anchor = 'w')
 countdown_timer_input = Entry(root, width = 20)
-countdown_timer_input.insert(0, "00:00:00")
+countdown_timer_input.insert(0, countdown_timer)
 
 alarm_time_label = Label(root, text = "Alarm timeout (ss)", width = 22, anchor = 'w')
 alarm_time_input = Entry(root, width = 20)
-alarm_time_input.insert(0, "10")
+alarm_time_input.insert(0, alarm_time)
 
-start_button = Button(root, text = "Save", command = lambda: save(trigger_key_input.get(), countdown_timer_input.get(), alarm_time_input.get()))
+info_label_1 = Label(root, text = 'To stop background process/trigger press: shift+x')
+info_label_2 = Label(root, text = 'To run again please press Trigger button again.')
+info_label_3 = Label(root, text = 'Created by @shashankm28 (Github/Instagram)')
+
+save_button = Button(root, text = "Save config", command = lambda: save(trigger_key_input.get(), countdown_timer_input.get(), alarm_time_input.get()))
+trigger_button = Button(root, text = "Trigger", command = trigger)
 
 trigger_key_label.grid(row = 0, column = 0, padx = 10, pady = 2.5)
 trigger_key_input.grid(row = 0, column = 1, padx = 10, pady = 2.5)
@@ -74,6 +125,14 @@ countdown_timer_label.grid(row = 1, column = 0, padx = 10, pady = 2.5)
 countdown_timer_input.grid(row = 1, column = 1, padx = 10, pady = 2.5)
 alarm_time_label.grid(row = 2, column = 0, padx = 10, pady = 2.5)
 alarm_time_input.grid(row = 2, column = 1, padx = 10, pady = 2.5)
-start_button.grid(row = 3, column = 1, padx = 10, pady = 2.5)
+save_button.grid(row = 3, column = 1, padx = 10, pady = 2.5, sticky = W)
+trigger_button.grid(row = 3, column = 1, padx = 10, pady = 2.5, sticky = E)
+
+info_label_1.grid(row = 4, column = 0, padx = 10, pady = 2.5, columnspan = 2 , sticky = W)
+info_label_2.grid(row = 5, column = 0, padx = 10, pady = 2.5, columnspan = 2 , sticky = W)
+info_label_3.grid(row = 6, column = 0, padx = 10, pady = 2.5, columnspan = 2 , sticky = W)
+
+status_label = Label(root, width = 57, text = "STATUS", bd = 1, relief = SUNKEN, anchor = W)
+status_label.grid(row = 7, column = 0, padx = 0, pady = 2.5, columnspan = 3 , sticky = W)
 
 root.mainloop()
